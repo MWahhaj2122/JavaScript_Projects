8af08a91e63c88668f3e50389dba569f56bd700d
MWahhaj2122 <m.wahhaj2122@gmail.com>
2020-08-18 10:53:21 +0500

    Update script.js
    Valiadate email  using simple JS

--- p01/script.js
+++ p01/script.js
@@ -9,14 +9,22 @@ const password1 =  document.getElementById('Password1');
 function showError(input,message) {
     const formControl = input.parentElement; // Used to access Parent Element as we are passing username as input only parent of username is accessed 
     formControl.className = 'form-control error';// We are assigning both error and form-control class to formControl
-    const small = formControl.querySelector('small');
-    small.innerText = message; 
+    const small = formControl.querySelector('small');// Select small element from FormControl
+    small.innerText = message; // OverWrite  the text of small to message.
 }
 //Show Success Message Function
 function showSuccess(input) {
     const formControl = input.parentElement; 
     formControl.className = 'form-control success';
 }
+// email Valiadation Function
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+
 
 //Event Listener for the form on submit
 form.addEventListener('submit',function(e){
@@ -32,8 +40,12 @@ e.preventDefault();
      {
       showError(email,'Email  is Invalid')
      }
+     else if(!isValidEmail(email.value)) // if email.value is true we donot want to execute this condition so, we use NOT(!),if email.value is false this statement becomes true and execute error block 
+     {
+      showError(email,'Email is not correct') 
+     }
      else{
-         showSuccess(email);
+      showSuccess(email);
      }
      if(password.value === '')
      {
@@ -41,7 +53,7 @@ e.preventDefault();
      }
      else{
          showSuccess(password);
-     }
+      }
      if(password1.value === '')
      {
       showError(password1,'Password donot match')
